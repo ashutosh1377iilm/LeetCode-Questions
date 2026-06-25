2e11214002552ca3990cb6b265c7a07f0317b31b
Ashutosh Kumar <ashutosh.25scs1003001377@iilm.edu>
2026-06-25 18:44:14 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -15,3 +15,19 @@ public:
         return maxProfit;
     }
 };
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int num = 0;
+//         for(int i=0;i<prices.size();i++){
+//             for(int j =i+1;j<prices.size();j++){
+//                 if(prices[i]<prices[j]){
+//                     if(num<prices[j]-prices[i]){
+//                         num = prices[j]-prices[i];
+//                     }
+//                 }
+//             }
+//         }
+//         return num;
+//     }
+// }; 
