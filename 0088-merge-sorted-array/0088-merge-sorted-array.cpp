71edcff4374d6459438748471e9a36076604281b
Ashutosh Kumar <ashutosh.25scs1003001377@iilm.edu>
2026-06-27 22:18:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (41.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,34 +1,64 @@
+// Method 1
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> merged(m+n);
         int i = m-1;
         int j = n-1;
         int k = m+n-1;
-
-        while(i >= 0 && j >= 0){
+        while(i>=0 && j>=0){
             if(nums1[i]<nums2[j]){
-                merged[k]=nums2[j];
+                nums1[k]=nums2[j];
                 j--;
             }
             else{
-                merged[k]=nums1[i];
+                nums1[k]=nums1[i];
                 i--;
             }
             k--;
         }
-        while(i>=0){
-            merged[k]=nums1[i];
-            i--;
-            k--;
-        }
         while(j>=0){
-            merged[k]=nums2[j];
+            nums1[k]=nums2[j];
             j--;
             k--;
         }
-        for(int i =0;i<m+n;i++){
-            nums1[i]=merged[i];
-        }
     }
 };
+
+// Method 1
+
+
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         vector<int> merged(m+n);
+//         int i = m-1;
+//         int j = n-1;
+//         int k = m+n-1;
+
+//         while(i >= 0 && j >= 0){
+//             if(nums1[i]<nums2[j]){
+//                 merged[k]=nums2[j];
+//                 j--;
+//             }
+//             else{
+//                 merged[k]=nums1[i];
+//                 i--;
+//             }
+//             k--;
+//         }
+//         while(i>=0){
+//             merged[k]=nums1[i];
+//             i--;
+//             k--;
+//         }
+//         while(j>=0){
+//             merged[k]=nums2[j];
+//             j--;
+//             k--;
+//         }
+//         for(int i =0;i<m+n;i++){
+//             nums1[i]=merged[i];
+//         }
+//     }
+// };
