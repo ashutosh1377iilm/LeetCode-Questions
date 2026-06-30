a97071688379bc004b97a27175e3f38604f056ce
Ashutosh Kumar <ashutosh.25scs1003001377@iilm.edu>
2026-06-30 10:32:06 +0530

    Time: 0 ms (100%), Space: 95.6 MB (33.49%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,37 +4,80 @@ public:
         int m = nums1.size();
         int n = nums2.size();
         int middleElement;
-        vector<int>merged(m+n);
+        vector<int>sortMerged(m+n);
         int i =0;
-        int j=0;
-        int k=0;
-        while(i<m&&j<n){
-            merged[k] = nums1[i];
-            i++;
-            k++;
-            merged[k]=nums2[j];
-            j++;
+        int j =0;
+        int k =0;
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){ 
+                sortMerged[k] = nums1[i];
+                i++;
+            }
+            else if(nums1[i]>=nums2[j]){
+                sortMerged[k] = nums2[j];
+                j++;
+            }
             k++;
         }
         while(i<m){
-            merged[k]=nums1[i];
+            sortMerged[k]=nums1[i];
             i++;
             k++;
         }
         while(j<n){
-            merged[k]=nums2[j];
+            sortMerged[k]=nums2[j];
             j++;
             k++;
         }
-        sort(merged.begin(),merged.end());
         if((m+n)%2!=0){
             middleElement= ((m+n)+1)/2;
-            return merged[middleElement-1];
+            return sortMerged[middleElement-1];
         }
         else{
             middleElement = (m+n)/2;
-             double even = merged[middleElement] +merged[middleElement-1];
+             double even = sortMerged[middleElement] +sortMerged[middleElement-1];
             return even/2;
         }
     }
 };
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size();
+//         int n = nums2.size();
+//         int middleElement;
+//         vector<int>merged(m+n);
+//         int i =0;
+//         int j=0;
+//         int k=0;
+//         while(i<m&&j<n){
+//             merged[k] = nums1[i];
+//             i++;
+//             k++;
+//             merged[k]=nums2[j];
+//             j++;
+//             k++;
+//         }
+//         while(i<m){
+//             merged[k]=nums1[i];
+//             i++;
+//             k++;
+//         }
+//         while(j<n){
+//             merged[k]=nums2[j];
+//             j++;
+//             k++;
+//         }
+//         sort(merged.begin(),merged.end());
+//         if((m+n)%2!=0){
+//             middleElement= ((m+n)+1)/2;
+//             return merged[middleElement-1];
+//         }
+//         else{
+//             middleElement = (m+n)/2;
+//              double even = merged[middleElement] +merged[middleElement-1];
+//             return even/2;
+//         }
+//     }
+// };
