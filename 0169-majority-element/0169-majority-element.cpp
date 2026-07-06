0f71851341feb9c7a765201c26d4613046544656
Ashutosh Kumar <ashutosh.25scs1003001377@iilm.edu>
2026-07-06 21:32:25 +0530

    Time: 2567 ms (5%), Space: 28.2 MB (66.88%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int majority= 0;
+        int element;
+        for(int i =0;i<n;i++){
+            int currentMajority=0;
+            for(int j =i;j<n;j++){
+                if(nums[i]==nums[j]) currentMajority++;
+            }
+            if(majority<currentMajority){
+                majority = currentMajority;
+                element=nums[i];
+            }
+        }
+    return element;
+    }
+};
